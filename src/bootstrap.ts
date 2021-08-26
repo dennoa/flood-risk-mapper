406c953640b1021853e3d8e0c504d853be1b8bf5
Andrew Dennison <me@example.com>
2021-08-26 15:13:28 +1000

    update

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,43 +1,43 @@
 /// <reference path="../typings/main.d.ts"/>
-import 'ts-helpers';
-import {enableProdMode} from '@angular/core';
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {ELEMENT_PROBE_PROVIDERS} from '@angular/platform-browser';
-import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
-import {HTTP_PROVIDERS} from '@angular/http';
-import {provideLazyMapsAPILoaderConfig, GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
+import "ts-helpers";
+import { enableProdMode } from "@angular/core";
+import { bootstrap } from "@angular/platform-browser-dynamic";
+import { ELEMENT_PROBE_PROVIDERS } from "@angular/platform-browser";
+import { ROUTER_PROVIDERS } from "@angular/router-deprecated";
+import { HTTP_PROVIDERS } from "@angular/http";
+import {
+  provideLazyMapsAPILoaderConfig,
+  GOOGLE_MAPS_PROVIDERS
+} from "angular2-google-maps/core";
 
 const ENV_PROVIDERS = [];
 // depending on the env mode, enable prod mode or add debugging modules
-if (process.env.ENV === 'prod') {
-    enableProdMode();
+if (process.env.ENV === "prod") {
+  enableProdMode();
 } else {
-    ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
+  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
 }
 
 /*
  * App Component
  * our top level component that holds all of our components
  */
-import {App} from './app/index';
+import { App } from "./app/index";
 
 /*
  * Bootstrap our Angular app with a top level component `App` and inject
  * our Services and Providers into Angular's dependency injection
  */
-document.addEventListener('DOMContentLoaded', function main() {
-    return bootstrap(App, [
-        // These are dependencies of our App
-        ...HTTP_PROVIDERS,
-        ...ROUTER_PROVIDERS,
-        ...ENV_PROVIDERS,
-        GOOGLE_MAPS_PROVIDERS,
-        provideLazyMapsAPILoaderConfig({ apiKey: 'AIzaSyAB3VwottxsX0DfgWphHisiyI2ZEy0FENU' })
-
-    ])
-        .catch(err => console.error(err));
+document.addEventListener("DOMContentLoaded", function main() {
+  return bootstrap(App, [
+    // These are dependencies of our App
+    ...HTTP_PROVIDERS,
+    ...ROUTER_PROVIDERS,
+    ...ENV_PROVIDERS,
+    GOOGLE_MAPS_PROVIDERS,
+    provideLazyMapsAPILoaderConfig({ apiKey: "my_key" })
+  ]).catch(err => console.error(err));
 });
-
 
 /*
  * Modified for using hot module reload
@@ -48,20 +48,18 @@ declare let module: any;
 
 // activate hot module reload
 if (module.hot) {
+  // bootstrap must not be called after DOMContentLoaded,
+  // otherwise it cannot be rerenderd after module replacement
+  //
+  // for testing try to comment the bootstrap function,
+  // open the dev tools and you'll see the reloader is replacing the module but cannot rerender it
+  bootstrap(App, [
+    ...ENV_PROVIDERS,
+    ...HTTP_PROVIDERS,
+    ...ROUTER_PROVIDERS,
+    GOOGLE_MAPS_PROVIDERS,
+    provideLazyMapsAPILoaderConfig({ apiKey: "my_key" })
+  ]).catch(err => console.error(err));
 
-    // bootstrap must not be called after DOMContentLoaded,
-    // otherwise it cannot be rerenderd after module replacement
-    //
-    // for testing try to comment the bootstrap function,
-    // open the dev tools and you'll see the reloader is replacing the module but cannot rerender it
-    bootstrap(App, [
-        ...ENV_PROVIDERS,
-        ...HTTP_PROVIDERS,
-        ...ROUTER_PROVIDERS,
-        GOOGLE_MAPS_PROVIDERS,
-        provideLazyMapsAPILoaderConfig({ apiKey: 'AIzaSyAB3VwottxsX0DfgWphHisiyI2ZEy0FENU' })
-    ])
-        .catch(err => console.error(err));
-
-    module.hot.accept();
+  module.hot.accept();
 }
